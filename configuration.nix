206b8e13dc9cff0f1817ede7c209393dbe314083
pongo1231 <pongo1999712@gmail.com>
2022-11-01 22:14:25 +0100

    Fix bluedevil not showing battery level without bluetooth service restart

--- configuration.nix
+++ configuration.nix
@@ -121,7 +121,7 @@ in
 
   hardware.bluetooth = {
     enable = true;
-    hsphfpd.enable = true;
+    powerOnBoot = false; # fixes --experimental flag not applying on boot
   };
   # show bluetooth headset battery level in kde
   systemd.services.bluetooth.serviceConfig.ExecStart = lib.mkForce [
