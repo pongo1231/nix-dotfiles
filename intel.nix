107d3a806d71cc2c4d717e35d8a08ca7c67b05a7
pongo1231 <pongo1999712@gmail.com>
2023-04-25 02:02:27 +0200

    hardware decode stuff

--- intel.nix
+++ intel.nix
@@ -1,14 +1,20 @@
 { pkgs
 , lib
 , ...
-}: {        
+}: {
   hardware.opengl = {
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
-    extraPackages = with pkgs.unstable; [
+    extraPackages = with pkgs; [
       intel-media-driver
+      vaapiIntel
+      libvdpau-va-gl
     ];
-    extraPackages32 = with pkgs.unstable.pkgsi686Linux; [ intel-media-driver ];
+    extraPackages32 = with pkgs.pkgsi686Linux; [ intel-media-driver vaapiIntel libvdpau-va-gl ];
+  };
+
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
   };
 }
