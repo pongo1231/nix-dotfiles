ad64ac73dd650940627571c7a69006a324cf1a0d
pongo1231 <pongo1999712@gmail.com>
2023-04-29 00:16:13 +0200

    use unstable video hardware accel decoding packages
    the 22.11 ones seem to be causing frequent crashes in firefox

--- intel.nix
+++ intel.nix
@@ -6,12 +6,12 @@
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
-    extraPackages = with pkgs; [
+    extraPackages = with pkgs.unstable; [
       intel-media-driver
       vaapiIntel
       libvdpau-va-gl
     ];
-    extraPackages32 = with pkgs.pkgsi686Linux; [ intel-media-driver vaapiIntel libvdpau-va-gl ];
+    extraPackages32 = with pkgs.unstable.pkgsi686Linux; [ intel-media-driver vaapiIntel libvdpau-va-gl ];
   };
 
   environment.sessionVariables = {
