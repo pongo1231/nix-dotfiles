681b63b716f8a5cbdb38d6e224dee450664e7bf5
pongo1231 <pongo1999712@gmail.com>
2023-07-19 18:57:06 +0200

    comment out nohz_full

--- configuration.nix
+++ configuration.nix
@@ -65,7 +65,7 @@
       "i915.enable_fbc=1"
       "i915.enable_gvt=1"
       "i915.enable_psr=1"
-      "nohz_full=1-3,5-7"
+      #"nohz_full=1-3,5-7"
       "workqueue.power_efficient=true"
     ];
     kernelPatches = [
