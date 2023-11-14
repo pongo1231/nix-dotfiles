f6cdc47f62f12edefd4c21619884c79046ef5d1e
pongo1231 <pongo1999712@gmail.com>
2023-11-14 21:54:18 +0100

    increase compression levels

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -16,7 +16,7 @@
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/bb7eda0c-17b1-4399-a3a0-fa1c72b45877";
     fsType = "btrfs";
-    options = [ "subvol=root" "noatime" "compress-force=zstd:6" ];
+    options = [ "subvol=root" "noatime" "compress-force=zstd:10" ];
   };
 
   boot.initrd.luks.devices = {
@@ -46,13 +46,13 @@
   fileSystems."/media/ssd" = {
     device = "/dev/disk/by-uuid/31b51531-24cb-4532-aef8-8c866f08e178";
     fsType = "btrfs";
-    options = [ "noatime" "compress-force=zstd:6" ];
+    options = [ "noatime" "compress-force=zstd:10" ];
   };
 
   fileSystems."/media/hdd" = {
     device = "/dev/disk/by-uuid/c3a302d7-34c8-42c3-98c9-f8e38f0ba245";
     fsType = "btrfs";
-    options = [ "noatime" "compress-force=zstd:10" "autodefrag" ];
+    options = [ "noatime" "compress-force=zstd:15" "autodefrag" ];
   };
 
   swapDevices = [ ];
