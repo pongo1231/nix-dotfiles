2af9fd4ce11ab66fb22fecf871eb9cd5bdcc2e65
pongo1231 <pongo1999712@gmail.com>
2023-11-28 23:21:46 +0100

    nitro5/libvirt: only create igvt-g instance if vm guest name contains "_igpu"

--- desktop/nitro5/libvirt.nix
+++ desktop/nitro5/libvirt.nix
@@ -52,6 +52,10 @@
     STATE_NAME="\$3"
     MISC="\''${@:4}"
 
+    if [[ $GUEST_NAME != *"_igpu"* ]]; then
+      exit 0
+    fi
+
     if [ \$HOOK_NAME = "prepare" ] && [ \$STATE_NAME = "begin" ]; then
       echo "286d3cce-2b6e-4e71-8045-8904caaa3ab0" > "/sys/bus/pci/devices/0000:00:02.0/mdev_supported_types/i915-GVTg_V5_4/create"
     elif [ \$HOOK_NAME = "release" ] && [ \$STATE_NAME = "end" ]; then
