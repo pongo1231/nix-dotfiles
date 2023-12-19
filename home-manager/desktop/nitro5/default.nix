1760f62eb789a9fe9ccebc557f0c6bb1d1427ab2
pongo1231 <pongo1999712@gmail.com>
2023-12-19 18:26:57 +0100

    home/nitro5/discord: enable ozone

--- home-manager/desktop/nitro5/default.nix
+++ home-manager/desktop/nitro5/default.nix
@@ -18,7 +18,7 @@
     intel-gpu-tools
     authy
     ((discord.override { nss = nss_latest; /* workaround to fix links not opening browsers */ }).overrideAttrs (prevAttrs: rec {
-      desktopItem = prevAttrs.desktopItem.override { exec = "Discord --disable-smooth-scrolling"; };
+      desktopItem = prevAttrs.desktopItem.override { exec = "Discord --disable-smooth-scrolling --enable-features=UseOzonePlatform --ozone-platform=wayland"; };
       installPhase = builtins.replaceStrings [ "${prevAttrs.desktopItem}" ] [ "${desktopItem}" ] prevAttrs.installPhase;
     }))
     libreoffice
