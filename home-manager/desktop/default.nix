e93aeb1a06f26509b2318628b1783127845e623d
pongo1231 <pongo1999712@gmail.com>
2023-12-23 21:35:48 +0100

    home: move appropriate stuff from common to desktop

--- home-manager/desktop/default.nix
+++ home-manager/desktop/default.nix
@@ -1,4 +1,54 @@
-{ ... }:
+{ config
+, pkgs
+, ... }:
 {
-  # ...
+  programs.firefox.profiles.settings = {
+    "gfx.webrender.all" = true;
+    "media.ffmpeg.vaapi.enabled" = true;
+  };
+
+  home = {
+    sessionVariables.MOZ_ENABLE_WAYLAND = "1";
+
+    pointerCursor = {
+      gtk.enable = true;
+      x11.enable = true;
+      package = pkgs.breeze-icons;
+      name = "Breeze";
+    };
+
+    file = {
+      # Wrapper for running flatpak inside steam-session
+      # Steam-rom-manager doesn't let us point to a non-existing file
+      # and the dir of the flatpak executable differs inside the FHS container :(
+      "flatpak.sh" = {
+        text = ''
+          #!/bin/sh
+          flatpak "''${@:1}"
+        '';
+        executable = true;
+      };
+
+      # Workaround for plasma-browser-integration
+      ".mozilla/native-messaging-hosts/org.kde.plasma.browser_integration.json".source = "${pkgs.plasma-browser-integration}/lib/mozilla/native-messaging-hosts/org.kde.plasma.browser_integration.json";
+    };
+
+    packages = with pkgs; [
+      firefox
+      kate
+      ark
+      ocs-url
+      nil
+      nixpkgs-fmt
+      filelight
+      vlc
+      manix
+      nvd
+      kdeconnect
+      ffmpeg
+      protontricks
+      appimage-run
+      maliit-keyboard
+    ];
+  };
 }
