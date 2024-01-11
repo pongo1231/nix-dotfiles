e844fad3aef1fd28023b33b9940b976f5095049e
pongo1231 <pongo1999712@gmail.com>
2024-01-11 15:54:33 +0100

    mesa-radv-jupiter: update rev

--- pkgs/mesa-radv-jupiter/default.nix
+++ pkgs/mesa-radv-jupiter/default.nix
@@ -1,14 +1,25 @@
 { mesa-radv-jupiter'
+, libdrm
 , fetchFromGitLab
+, fetchurl
 }:
 
-mesa-radv-jupiter'.overrideAttrs (finalAttrs: prevAttrs: {
+(mesa-radv-jupiter'.override {
+  libdrm = libdrm.overrideAttrs (finalAttrs: prevAttrs: {
+    version = "2.4.119";
+
+    src = fetchurl {
+      url = "https://dri.freedesktop.org/${finalAttrs.pname}/${finalAttrs.pname}-${finalAttrs.version}.tar.xz";
+      hash = "sha256-CknxLwm1tuaOqq/z8Cynz/mqkmk5shLTQxYdPorFYpE=";
+    };
+  });
+}).overrideAttrs (prevAttrs: {
   src = fetchFromGitLab {
     domain = "gitlab.freedesktop.org";
     owner = "Mesa";
     repo = "mesa";
-    rev = "c511b8968a28c0c98a36500f68881cde0d2104bd";
-    hash = "sha256-Xe+PRr/cRwXuBkCtaIoyhdc+A60XwXwNAmD8QnI03eM=";
+    rev = "c31be1f4bacde88ccd7177af26cb554c35472573";
+    hash = "sha256-quePEV37HJOSVWun90dkteO0r5yuxnDLmcfZlZ5lYUA=";
   };
 
   patches = prevAttrs.patches ++ [ ./25352.diff ];
