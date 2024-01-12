1aa0f23b0e539a51f7f98d5114cd196d81986d45
pongo1231 <pongo1999712@gmail.com>
2024-01-12 18:06:00 +0100

    mesa-radv-jupiter: update rev

--- pkgs/mesa-radv-jupiter/default.nix
+++ pkgs/mesa-radv-jupiter/default.nix
@@ -18,8 +18,8 @@
     domain = "gitlab.freedesktop.org";
     owner = "Mesa";
     repo = "mesa";
-    rev = "c31be1f4bacde88ccd7177af26cb554c35472573";
-    hash = "sha256-quePEV37HJOSVWun90dkteO0r5yuxnDLmcfZlZ5lYUA=";
+    rev = "2fa1979c6688b92917e6242bab7e5c08ec8831b4";
+    hash = "sha256-uHM+c3s7fNZXWDTmzYrhm6hx5/Obr1li4qb/ximi/rY=";
   };
 
   patches = prevAttrs.patches ++ [ ./25352.diff ];
