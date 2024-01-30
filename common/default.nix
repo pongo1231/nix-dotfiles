19a920afde5f4cbea0d65a34dce45c6951d640c2
pongo1231 <pongo1999712@gmail.com>
2024-01-30 19:59:07 +0100

    common: replace 0uptime.cachix.org substituter with nix-community.cachix.org

--- common/default.nix
+++ common/default.nix
@@ -20,10 +20,10 @@
       auto-optimise-store = true;
       trusted-users = [ "root" "@wheel" ];
       substituters = [
-        "https://0uptime.cachix.org"
+        "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
-        "0uptime.cachix.org-1:ctw8yknBLg9cZBdqss+5krAem0sHYdISkw/IFdRbYdE="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
     gc = {
