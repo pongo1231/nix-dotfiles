0cd0100d839faf0e3d78a3f7b8f79fafe22762b1
pongo1231 <pongo1999712@gmail.com>
2024-02-03 22:18:06 +0100

    mesa-radv-jupiter: adjust for jovian change

--- pkgs/mesa-radv-jupiter/default.nix
+++ pkgs/mesa-radv-jupiter/default.nix
@@ -1,19 +1,9 @@
 { mesa-radv-jupiter'
-, libdrm
 , fetchFromGitLab
 , fetchurl
 }:
 
-(mesa-radv-jupiter'.override {
-  libdrm = libdrm.overrideAttrs (finalAttrs: prevAttrs: {
-    version = "2.4.119";
-
-    src = fetchurl {
-      url = "https://dri.freedesktop.org/${finalAttrs.pname}/${finalAttrs.pname}-${finalAttrs.version}.tar.xz";
-      hash = "sha256-CknxLwm1tuaOqq/z8Cynz/mqkmk5shLTQxYdPorFYpE=";
-    };
-  });
-}).overrideAttrs (prevAttrs:
+mesa-radv-jupiter'.overrideAttrs (prevAttrs:
 let
   rev = "559f31e202fd8bdb984a6bbf8c03d65dd1e93d57";
 in
