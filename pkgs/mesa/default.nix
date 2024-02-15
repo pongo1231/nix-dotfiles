835a0fea0ea4705bba01ad35c35e21992d32b3c2
pongo1231 <pongo1999712@gmail.com>
2024-02-15 19:56:32 +0100

    mesa: unfuck

--- pkgs/mesa/default.nix
+++ pkgs/mesa/default.nix
@@ -1,12 +1,29 @@
-{ mesa
+{ lib
+, mesa
+, libclc
+, llvmPackages
 , fetchFromGitLab
 , fetchurl
 , fetchpatch
 }:
 
-mesa.overrideAttrs (prevAttrs:
+(mesa.override {
+  galliumDrivers = [
+    "iris"
+    "i915"
+    "radeonsi"
+    "swrast"
+  ];
+  vulkanDrivers = [
+    "intel"
+    "amd"
+    "swrast"
+  ];
+  enableGalliumNine = false;
+  enableOpenCL = true;
+}).overrideAttrs (prevAttrs:
 let
-  rev = "5178ad761c9e8e86ffb3bc59322ec998c0ae2063";
+  rev = "90eae30bcb84d54dc871ddbb8355f729cf8fa900";
 in
 {
   version = "git-${builtins.substring 0 6 rev}";
@@ -16,10 +33,10 @@ in
     owner = "Mesa";
     repo = "mesa";
     inherit rev;
-    hash = "sha256-JcNXhSsjC3oI5efOkDHG4P9BVshFIajoui8EaoAYr7M=";
+    hash = "sha256-EJihKqmLHJmIgRpKx5lTTDlFoT5jCEFLQlJYTYifJLY=";
   };
 
-  patches = prevAttrs.patches ++ [
+  patches = (builtins.filter (x: !lib.strings.hasInfix "000" x /* skip macOS backports */) prevAttrs.patches) ++ [
     (fetchpatch {
       url = "https://github.com/Jovian-Experiments/mesa/commit/9ca7d7775f34bca9578306b139ddd03e4f176e01.patch";
       hash = "sha256-dVqt0x0yDyNfpBKOo/tRk8394PPVOooNTpCLsFqZwUE=";
