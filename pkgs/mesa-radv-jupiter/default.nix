72f1587c1ab66eac38a2534b4d4f2b6dd35edbac
pongo1231 <pongo1999712@gmail.com>
2024-02-24 00:14:43 +0100

    mesa-radv-jupiter: update rev

--- pkgs/mesa-radv-jupiter/default.nix
+++ pkgs/mesa-radv-jupiter/default.nix
@@ -7,7 +7,7 @@
 
 (mesa-radv-jupiter'.overrideAttrs (prevAttrs:
 let
-  rev = "90eae30bcb84d54dc871ddbb8355f729cf8fa900";
+  rev = "85849ed9cb6ee802ae6917d8fa78cca86b41fb85";
 in
 {
   version = "git-${builtins.substring 0 6 rev}";
@@ -17,7 +17,7 @@ in
     owner = "Mesa";
     repo = "mesa";
     inherit rev;
-    hash = "sha256-EJihKqmLHJmIgRpKx5lTTDlFoT5jCEFLQlJYTYifJLY=";
+    hash = "sha256-Jxx53T87i/u56Mk8tobYVX3JJn5o9MUCa0zatYjSwtM=";
   };
 
   patches = (builtins.filter (x: !lib.strings.hasInfix "000" x /* skip macOS backports */) prevAttrs.patches) ++ [
@@ -26,5 +26,6 @@ in
 
   mesonFlags = (builtins.filter (x: !lib.strings.hasInfix "-Dintel-clc" x) prevAttrs.mesonFlags) ++ [
     "-Dintel-clc=system"
+    "-Dintel-rt=disabled"
   ];
 }))
