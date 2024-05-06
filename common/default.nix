309ac95b6df89e61b4c2345debd807c2b4cfafcc
pongo1231 <pongo1999712@gmail.com>
2024-05-06 22:07:36 +0200

    common: add pongo to video group

--- common/default.nix
+++ common/default.nix
@@ -159,7 +159,7 @@
     users.pongo = {
       isNormalUser = true;
       home = "/home/pongo";
-      extraGroups = [ "wheel" "input" "libvirtd" "networkmanager" "podman" ];
+      extraGroups = [ "wheel" "input" "libvirtd" "networkmanager" "podman" "video" ];
       hashedPassword = "$6$jTFwtF9QaSc/j2sI$W9nNE/f6QK1NE3uinzPYBffvxck86lmKf772auIG/8uESh.H1U9ZUUndd.DpW0tZKWOehfpJOxnGOVIxqmvh00";
     };
   };
