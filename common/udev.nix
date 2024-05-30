20cce5ab916ba04b364bff8726005d6c18d5428a
pongo1231 <pongo1999712@gmail.com>
2024-05-30 23:44:51 +0200

    common: enable runtime pm for all pci devices

--- common/udev.nix
+++ common/udev.nix
@@ -6,5 +6,8 @@
     ACTION=="add|change", KERNEL=="sd[a-z]*|mmcblk[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="mq-deadline"
     # set scheduler for rotating disks
     ACTION=="add|change", KERNEL=="sd[a-z]*", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="bfq"
+
+    # auto enable runtime pm for all pci devices
+    SUBSYSTEM=="pci", ATTR{power/control}="auto"
   '';
 }
