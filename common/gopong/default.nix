57d2bd0a5fb8e4cb8e870f97d1f081a17980157a
pongo1231 <pongo1999712@gmail.com>
2024-06-20 23:34:55 +0200

    gopong: init

---
+++ common/gopong/default.nix
@@ -0,0 +1,25 @@
+{ config
+, lib
+, pkgs
+, modulesPath
+, ...
+}:
+{
+  boot = {
+    initrd.availableKernelModules = [ "ata_piix" "virtio_pci" "virtio_scsi" "xhci_pci" "sd_mod" "sr_mod" ];
+    loader = {
+      systemd-boot.enable = lib.mkForce false;
+      grub = {
+        enable = true;
+        device = "/dev/sda";
+      };
+    };
+  };
+
+  fileSystems."/" =
+    {
+      device = "/dev/disk/by-uuid/73d20b49-f05c-4d6a-9cf6-f6d6c90a54a2";
+      fsType = "btrfs";
+      options = [ "noatime" "compress-force=zstd:6" ];
+    };
+}
