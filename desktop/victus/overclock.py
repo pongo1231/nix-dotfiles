22f378b0208079b7865d92d294a1578dcbaa54ae
pongo1231 <pongo1999712@gmail.com>
2024-06-22 01:50:31 +0200

    victus: update overclock.py

--- desktop/victus/overclock.py
+++ desktop/victus/overclock.py
@@ -3,14 +3,8 @@
 from pynvml import *
 nvmlInit()
 
-# This sets the GPU to adjust - if this gives you errors or you have multiple GPUs, set to 1 or try other values
 myGPU = nvmlDeviceGetHandleByIndex(0)
 
-# The GPU frequency offset value should replace "80" in the line below.
-nvmlDeviceSetGpcClkVfOffset(myGPU, 235)
-# safe: 235
+nvmlDeviceSetGpcClkVfOffset(myGPU, 220)
 
-# The Mem frequency Offset should be **multiplied by 2** to replace the "2500" below
-# for example, an offset of 500 in GWE means inserting a value of 1000 in the next line
-nvmlDeviceSetMemClkVfOffset(myGPU, 3650)
-# safe: 3650
+nvmlDeviceSetMemClkVfOffset(myGPU, 3500)
