4f04cd7788ee9e4cc9b638c0a04e7ccb7fedb7e1
pongo1231 <pongo1999712@gmail.com>
2024-06-28 12:46:19 +0200

    wsl: add graphics drivers

--- common/wsl/default.nix
+++ common/wsl/default.nix
@@ -17,6 +17,14 @@
     startMenuLaunchers = true;
   };
 
+  hardware.graphics = {
+    extraPackages = with pkgs; [
+      mesa.drivers
+      libvdpau-va-gl
+      (libedit.overrideAttrs (attrs: {postInstall = (attrs.postInstall or "") + ''ln -s $out/lib/libedit.so $out/lib/libedit.so.2'';}))
+    ];
+  };
+
   systemd.oomd.enable = lib.mkForce false;
 
   boot.loader.systemd-boot.enable = lib.mkForce false;
