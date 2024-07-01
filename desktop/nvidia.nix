06be7f668e93252caa6a05e823634ae1e99112bc
pongo1231 <pongo1999712@gmail.com>
2024-07-01 21:04:25 +0200

    nvidia: back to proprietary yet again

--- desktop/nvidia.nix
+++ desktop/nvidia.nix
@@ -35,7 +35,7 @@
         })).nvidiaPackages.production;
     modesetting.enable = true;
     #nvidiaPersistenced = true;
-    open = true;
+    open = false;
     prime = {
       offload.enable = true;
       nvidiaBusId = "PCI:1:0:0";
