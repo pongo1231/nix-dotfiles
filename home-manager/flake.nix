d5471afeb52d29f4cc718389f4941b256f2ea30d
pongo1231 <pongo1999712@gmail.com>
2024-08-24 23:34:17 +0200

    home/flake/nixpkgs: also switch to personal fork

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:pongo1231/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
