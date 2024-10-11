ab8ac9ea6f7161b81f058670c68491984c2e7ea5
pongo1231 <pongo1999712@gmail.com>
2024-10-12 00:10:33 +0200

    wicked_kernel: init

---
+++ modules/wicked_kernel.nix
@@ -0,0 +1,95 @@
+{ pkgs, lib, ... }:
+{
+  nixpkgs.overlays = [
+    (final: prev: {
+      linuxPackages_wicked = (final.kernel.linuxPackages_testing.extend (finalAttrs: prevAttrs: {
+        kernel = prevAttrs.kernel.override (prevAttrs': {
+          #kernelPatches = builtins.filter (x: !lib.hasPrefix "rust" x.name) prevAttrs'.kernelPatches;
+          ignoreConfigErrors = true;
+          argsOverride =
+            let
+              version = "6.12-rc2";
+            in
+            {
+              inherit version;
+              modDirVersion = "6.12.0-rc2";
+              /*src = pkgs.fetchgit {
+                url = "https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git";
+                rev = "eee280841e1c8188fe9af5536c193d07d184e874";
+                hash = "sha256-DbCBWmOHQA/h/RlLackDqfeauK4werdwk+zkSywRkxE=";
+                };*/
+              src = pkgs.fetchzip {
+                url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
+                hash = "sha256-3SREZ3EHC0poAH3CyA0HY5xnh8lbCIl3S6cqZx3ckbM=";
+              };
+            };
+        });
+      }));
+    })
+  ];
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_wicked;
+
+    kernelPatches =
+      let
+        # from https://gist.github.com/al3xtjames/a9aff722b7ddf8c79d6ce4ca85c11eaa
+        decode = pkgs.writeShellScript "
+                  decodeMbox " ''
+          export PATH="${lib.makeBinPath [ pkgs.git ]}:$PATH"
+          export XDG_DATA_HOME="$TMPDIR"
+          gzip -dc | ${pkgs.b4}/bin/b4 -n --offline-mode am -m - -o -
+        '';
+      in
+      [
+        /*{
+          name = "dgpu passthrough fix";
+          patch = null;
+          extraConfig = ''
+            HSA_AMD_SVM n
+          '';
+        }*/
+        {
+          name = "Fix 6.12 build";
+          patch = null;
+          extraConfig = ''
+            I2C_DESIGNWARE_PLATFORM m
+          '';
+        }
+        /*{
+          name = "no-latency-multiplier";
+          patch = pkgs.fetchpatch {
+            url = "https://lore.kernel.org/linux-pm/20240728192659.58115-1-qyousef@layalina.io/t.mbox";
+            hash = "sha256-kDKpSmZflv0B0023W35Gm9F3D8BYfiltLOrDMxQS23s=";
+            inherit decode;
+          };
+        }*/
+        /*{
+          name = "amdgpu-perf-fix";
+          patch = ../../patches/linux/drm-fixes-2024-09-06.patch;
+         }*/
+        /*{
+          name = "shrink-file-struct";
+          patch = ../../patches/linux/shrink-file-struct.patch;
+        }*/
+        /*{
+          name = "bore-6.11";
+          patch = pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/firelzrd/bore-scheduler/refs/heads/main/patches/testing/linux-6.11-bore/0001-linux6.11.y-bore5.3.0-rc4.patch";
+            hash = "sha256-nm+IxyxbsKKueJBLbkkrY7raI64MRnwV0g5WwXQSMF0=";
+          };
+        }*/
+        {
+          name = "preempt-lazy";
+          patch = pkgs.fetchpatch {
+            url = "https://lore.kernel.org/all/20241009110733.5S5z_mq0@linutronix.de/t.mbox";
+            hash = "sha256-S455vwyf9cnLMHquE2CnFg3n/2VCa5+C3ukZpiK2gLg=";
+            inherit decode;
+          };
+          extraConfig = ''
+            PREEMPT_LAZY y
+          '';
+        }
+      ];
+  };
+}
