b6ca092ffc2694038d349eb4d035c5e001926103
pongo1231 <pongo1999712@gmail.com>
2024-10-29 13:08:25 +0100

    common: increase swappiness to 200

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -64,7 +64,7 @@
     kernel = {
       sysctl = {
         # https://wiki.archlinux.org/title/Zram#Optimizing_swap_on_zram
-        "vm.swappiness" = 180;
+        "vm.swappiness" = 200;
         "vm.page-cluster" = 0;
         "vm.watermark_boost_factor" = 0;
         "vm.watermark_scale_factor" = 125;
