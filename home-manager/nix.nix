64dbc67a454e1a3d68d629ae7b7d5e70f7a51116
pongo1231 <pongo1999712@gmail.com>
2024-11-10 17:39:46 +0100

    Revert "nix: use trusted-substituters & remove cache.nixos.org from explicit list"
    This reverts commit 5a7b1415d8cb9c04fa66c2c77622087d02c53896.

--- home-manager/nix.nix
+++ home-manager/nix.nix
@@ -7,29 +7,25 @@
 {
   nix = {
     package = lib.mkDefault pkgs.lix;
-
     extraOptions = ''
       experimental-features = ca-derivations nix-command flakes
       keep-outputs = true
       keep-derivations = true
     '';
-
     settings = {
       auto-optimise-store = true;
-
       trusted-users = [ "root" "@wheel" ];
-
-      trusted-substituters = [
+      substituters = [
+        "https://cache.nixos.org"
         "https://nix-community.cachix.org"
         "https://pongo1231.cachix.org"
       ];
-
       trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "pongo1231.cachix.org-1:3B6q/T1NL/YPokIFY4lthjoI6vCMKiuYjTGY3gJtZPg="
       ];
     };
-
     registry = lib.mapAttrs' (name: flake: { inherit name; value.flake = flake; }) inputs;
   };
 }
