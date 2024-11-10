5a7b1415d8cb9c04fa66c2c77622087d02c53896
pongo1231 <pongo1999712@gmail.com>
2024-11-10 04:42:22 +0100

    nix: use trusted-substituters & remove cache.nixos.org from explicit list

--- home-manager/nix.nix
+++ home-manager/nix.nix
@@ -7,25 +7,29 @@
 {
   nix = {
     package = lib.mkDefault pkgs.lix;
+
     extraOptions = ''
       experimental-features = ca-derivations nix-command flakes
       keep-outputs = true
       keep-derivations = true
     '';
+
     settings = {
       auto-optimise-store = true;
+
       trusted-users = [ "root" "@wheel" ];
-      substituters = [
-        "https://cache.nixos.org"
+
+      trusted-substituters = [
         "https://nix-community.cachix.org"
         "https://pongo1231.cachix.org"
       ];
+
       trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "pongo1231.cachix.org-1:3B6q/T1NL/YPokIFY4lthjoI6vCMKiuYjTGY3gJtZPg="
       ];
     };
+
     registry = lib.mapAttrs' (name: flake: { inherit name; value.flake = flake; }) inputs;
   };
 }
