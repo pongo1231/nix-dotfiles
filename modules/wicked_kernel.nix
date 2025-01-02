2c08788bcace65b944e7f7618962972543a22c0f
pongo1231 <pongo1999712@gmail.com>
2025-01-02 11:22:32 +0100

    wicked_kernel: update bore patch

--- modules/wicked_kernel.nix
+++ modules/wicked_kernel.nix
@@ -216,7 +216,7 @@
         }*/
         {
           name = "BORE";
-          patch = patch /linux/6.13/0001-linux6.13.y-bore5.9.3.patch;
+          patch = patch /linux/6.13/0001-linux6.13.y-bore5.9.5.patch;
         }
         {
           name = "mglru-optimizations";
