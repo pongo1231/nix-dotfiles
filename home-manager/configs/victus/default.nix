5fea644b78083149a0c0b9d4e9eee9ccfd2d8c8c
pongo1231 <pongo1999712@gmail.com>
2025-02-09 23:04:39 +0100

    home/victus: add strawberry

--- home-manager/configs/victus/default.nix
+++ home-manager/configs/victus/default.nix
@@ -1,5 +1,7 @@
 {
   module,
+  patch,
+  pkgs,
   ...
 }:
 {
@@ -8,5 +10,19 @@
       "amd"
       "nvidia"
     ])
+  ];
+
+  home.packages = with pkgs; [
+    (strawberry.overrideAttrs (
+      finalAttrs: prevAttrs: {
+        patches = (prevAttrs.patches or [ ]) ++ [
+          (patch /strawberry/1467.patch)
+        ];
+        buildInputs = with pkgs; prevAttrs.buildInputs ++ [ projectm ];
+        postPatch = ''
+substituteInPlace src/visualizations/projectmvisualization.cpp --replace-fail "QStringList data_paths = QStringList() << QStringLiteral(\"/usr/share\")" "QStringList data_paths = QStringList() << QStringLiteral(\"${pkgs.projectm}/share\")"
+        '';
+      }
+    ))
   ];
 }
