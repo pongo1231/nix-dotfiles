98aad4d2c63656c59950f4e1fa0a871e476a7465
pongo1231 <pongo1999712@gmail.com>
2025-02-11 22:42:46 +0100

    home/duperemove: update

--- home-manager/overlay.nix
+++ home-manager/overlay.nix
@@ -1,24 +1,18 @@
-{ }:
+{ ... }:
 (final: prev: {
   duperemove = prev.duperemove.overrideAttrs (
     finalAttrs: prevAttrs: {
       src = final.fetchFromGitHub {
         owner = "markfasheh";
         repo = "duperemove";
-        rev = "c389d3d5309ed5641aae8cb5d7a255019396bf86";
-        hash = "sha256-5yyeHGttSlVro+j72VUBoscwIPd4scsQ8X2He4xWFJU=";
+        rev = "cacc30dcf4cd474329abdba08aa1fa8089b019e6";
+        hash = "sha256-lpQLa2bnUm8L17uQCON5X//LeL2CAj7GcAA/YWzqgGY=";
       };
 
       nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [
         final.libbsd
         final.xxHash
       ];
-
-      postPatch = ''
-        substituteInPlace Makefile --replace "--std=c23" "--std=c2x"
-        substituteInPlace results-tree.h --replace "// TODO: delete this" "#include \"list.h\""
-        substituteInPlace results-tree.h --replace "struct list_head {" "struct list_head_b {"
-      '';
     }
   );
 })
