10d7682198a31887bce006ec5ebf1ffe5e3ed9db
pongo1231 <pongo1999712@gmail.com>
2025-02-22 17:00:53 +0100

    home/victus: replace strawberry with audacious again

--- home-manager/configs/victus/default.nix
+++ home-manager/configs/victus/default.nix
@@ -13,16 +13,6 @@
   ];
 
   home.packages = with pkgs; [
-    (strawberry.overrideAttrs (
-      finalAttrs: prevAttrs: {
-        patches = (prevAttrs.patches or [ ]) ++ [
-          (patch /strawberry/1467.patch)
-        ];
-        buildInputs = with pkgs; prevAttrs.buildInputs ++ [ projectm ];
-        postPatch = ''
-          substituteInPlace src/visualizations/projectmvisualization.cpp --replace-fail "QStringList data_paths = QStringList() << QStringLiteral(\"/usr/share\")" "QStringList data_paths = QStringList() << QStringLiteral(\"${pkgs.projectm}/share\")"
-        '';
-      }
-    ))
+    audacious
   ];
 }
