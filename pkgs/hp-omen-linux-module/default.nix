06b3d403bd215f8d98fbda58267dff32de1a0306
pongo1231 <pongo1999712@gmail.com>
2025-03-14 18:12:00 +0100

    hp-omen-linux-module: update for 6.14

--- pkgs/hp-omen-linux-module/default.nix
+++ pkgs/hp-omen-linux-module/default.nix
@@ -5,13 +5,13 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "hp-omen-linux-module";
-  version = "rebase-6.12";
+  version = "rebase-6.14";
 
   src = fetchFromGitHub {
     owner = "ranisalt";
     repo = "hp-omen-linux-module";
     rev = finalAttrs.version;
-    sha256 = "sha256-EfOjKSgLbOysctaR+X7DJR2SdfAcUi4R/cXhVHrEw4M=";
+    sha256 = "sha256-2zCm29bdboSjRm/caMjBPGNc0tZXPUnIIYlHxxfhAok=";
   };
 
   setSourceRoot = ''
@@ -21,23 +21,14 @@ stdenv.mkDerivation (finalAttrs: {
   nativeBuildInputs = kernel.moduleBuildDependencies;
 
   makeFlags = [
-    #"TARGET=${kernel.modDirVersion}"
     "KERNELDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
-    #"-C"
-    #"${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
-    #"M=$(sourceRoot)"
-    #"VERSION=${finalAttrs.version}"
   ];
 
   installPhase = ''
     runHook preInstall
-
+    
     install hp-wmi.ko -Dm444 -t $out/lib/modules/${kernel.modDirVersion}/kernel/drivers/platform/x86/hp/
-
+    
     runHook postInstall
   '';
-
-  #buildFlags = [ "modules" ];
-  #installFlags = [ "INSTALL_MOD_PATH=${placeholder "out"}" ];
-  #installTargets = [ "modules_install" ];
 })
