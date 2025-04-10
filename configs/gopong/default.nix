762b2160460cecb9d65df23b597b6b47109f69d0
pongo1231 <pongo1999712@gmail.com>
2025-04-10 14:15:05 +0200

    gopong: add user stuff to nginx group

--- configs/gopong/default.nix
+++ configs/gopong/default.nix
@@ -44,7 +44,10 @@
   users.users = {
     stuff = {
       isNormalUser = true;
-      extraGroups = [ "podman" ];
+      extraGroups = [
+        "podman"
+        "nginx"
+      ];
       hashedPassword = "$y$j9T$c2Nt1td.mLWHzJt2GuFhG.$.QLj0Uh94KxKKq15MKXG6EUNJRt9N/AH7cSf2ABsQZ5";
     };
 
