f425dca2532d05d0c7b32220bb1beff341e35626
pongo1231 <pongo1999712@gmail.com>
2025-04-10 10:49:42 +0200

    gopong: add webserver.nix

--- configs/gopong/default.nix
+++ configs/gopong/default.nix
@@ -1,10 +1,12 @@
 {
-  module,
-  pkgs,
   lib,
   ...
 }:
 {
+  imports = [
+    ./webserver.nix
+  ];
+
   boot = {
     initrd.availableKernelModules = [
       "ata_piix"
@@ -35,52 +37,6 @@
 
   networking.networkmanager.enable = lib.mkForce false;
 
-  services.nginx = {
-    enable = true;
-    recommendedOptimisation = true;
-    recommendedTlsSettings = true;
-    recommendedProxySettings = true;
-    virtualHosts = {
-      "_" = {
-        rejectSSL = true;
-        globalRedirect = "gopong.dev";
-      };
-
-      "gopong.dev" = {
-        forceSSL = true;
-        enableACME = true;
-        root = "/srv/http";
-      };
-
-      "tf.gopong.dev" = {
-        rejectSSL = true;
-        extraConfig = ''
-          autoindex on;
-        '';
-        root = "/srv/http/tf";
-      };
-
-      "chaos.gopong.dev" = {
-        forceSSL = true;
-        enableACME = true;
-        locations."/".proxyPass = "https://gopong.dev:9907";
-        extraConfig = ''
-          client_max_body_size 50M;
-        '';
-      };
-
-      "habbo.gopong.dev" = {
-        forceSSL = true;
-        enableACME = true;
-        locations."/".proxyPass = "https://gopong.dev:8081";
-      };
-    };
-  };
-
-  security.acme = {
-    acceptTerms = true;
-    defaults.email = "pongo1999712@gmail.com";
-  };
 
   users = {
     users.habbo = {
