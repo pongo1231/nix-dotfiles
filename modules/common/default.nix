87b381ba4b7db3264e4b441367b309cb681dcafe
pongo1231 <pongo1999712@gmail.com>
2025-04-16 13:05:15 +0200

    common/mglru: min_ttl_ms to 1000

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -205,7 +205,9 @@
       "w /sys/kernel/mm/ksm/advisor_mode - - - - scan-time"
       "w /sys/kernel/mm/transparent_hugepage/khugepaged/max_ptes_none - - - - 409"
       "w /sys/class/rtc/rtc0/max_user_freq - - - - 3072"
-      "w /proc/sys/dev/hpet/max-user-freq  - - - - 3072"
+      "w /proc/sys/dev/hpet/max-user-freq - - - - 3072"
+
+      "w /sys/kernel/mm/lru_gen/min_ttl_ms - - - - 1000"
     ];
 
     services = {
