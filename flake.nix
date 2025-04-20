0cbc050dcb52ad6a61d402ea0b8df02299d29779
pongo1231 <pongo1999712@gmail.com>
2025-04-20 17:48:53 +0200

    flake: remove unused lix input

--- flake.nix
+++ flake.nix
@@ -35,11 +35,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    lix = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0-3.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     mailserver = {
       url = "gitlab:simple-nixos-mailserver/nixos-mailserver";
       inputs.nixpkgs.follows = "nixpkgs";
