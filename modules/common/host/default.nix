ee8ad8e42811e730370767f1d11fa11c8941d7fb
pongo1231 <pongo@gopong.dev>
2025-05-12 12:18:28 +0200

    common: remove min_ttl_ms tweak

--- modules/common/host/default.nix
+++ modules/common/host/default.nix
@@ -201,9 +201,6 @@
         "w /sys/kernel/mm/transparent_hugepage/khugepaged/max_ptes_none - - - - 409"
         "w /sys/class/rtc/rtc0/max_user_freq - - - - 3072"
         "w /proc/sys/dev/hpet/max-user-freq - - - - 3072"
-      ]
-      ++ lib.optionals (!config.pongo.pongoKernel.enable) [
-        "w /sys/kernel/mm/lru_gen/min_ttl_ms - - - - 1000"
       ];
 
     services = {
