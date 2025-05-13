6b972c0c6539f435ca86bcfb233eff7f35ad9148
pongo1231 <pongo@gopong.dev>
2025-05-14 00:55:44 +0200

    treewide: format

--- modules/common/host/default.nix
+++ modules/common/host/default.nix
@@ -196,12 +196,11 @@
       enableUserSlices = true;
     };
 
-    tmpfiles.rules =
-      [
-        "w /sys/kernel/mm/transparent_hugepage/khugepaged/max_ptes_none - - - - 409"
-        "w /sys/class/rtc/rtc0/max_user_freq - - - - 3072"
-        "w /proc/sys/dev/hpet/max-user-freq - - - - 3072"
-      ];
+    tmpfiles.rules = [
+      "w /sys/kernel/mm/transparent_hugepage/khugepaged/max_ptes_none - - - - 409"
+      "w /sys/class/rtc/rtc0/max_user_freq - - - - 3072"
+      "w /proc/sys/dev/hpet/max-user-freq - - - - 3072"
+    ];
 
     services = {
       "user@".serviceConfig = {
