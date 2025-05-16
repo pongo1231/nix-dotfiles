28f00527dab3850ecbe74c9cff979bd1f72f8ded
pongo1231 <pongo@gopong.dev>
2025-05-16 11:08:06 +0200

    pongoKernel: enable udev rules again

--- modules/common/host/pongoKernel.nix
+++ modules/common/host/pongoKernel.nix
@@ -134,15 +134,13 @@ in
       kernelModules = [ "adios" ];
     };
 
-    /*
-      services.udev.extraRules = ''
-        # set scheduler for NVMe
-        ACTION=="add|change", KERNEL=="nvme[0-9]n[0-9]", ATTR{queue/scheduler}="adios"
-        # set scheduler for SSD and eMMC
-        ACTION=="add|change", KERNEL=="sd[a-z]*|mmcblk[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="adios"
-        # set scheduler for rotating disks
-        ACTION=="add|change", KERNEL=="sd[a-z]*", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="adios"
-      '';
-    */
+    services.udev.extraRules = ''
+      # set scheduler for NVMe
+      ACTION=="add|change", KERNEL=="nvme[0-9]n[0-9]", ATTR{queue/scheduler}="adios"
+      # set scheduler for SSD and eMMC
+      ACTION=="add|change", KERNEL=="sd[a-z]*|mmcblk[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="adios"
+      # set scheduler for rotating disks
+      ACTION=="add|change", KERNEL=="sd[a-z]*", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="adios"
+    '';
   };
 }
