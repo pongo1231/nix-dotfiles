d19d583083479a830c3b7e8bf60888f7ad684eef
pongo1231 <pongo@gopong.dev>
2025-05-18 19:43:34 +0200

    common: min_ttl_ms to 500

--- modules/common/host/default.nix
+++ modules/common/host/default.nix
@@ -200,6 +200,8 @@
       "w /sys/kernel/mm/transparent_hugepage/khugepaged/max_ptes_none - - - - 409"
       "w /sys/class/rtc/rtc0/max_user_freq - - - - 3072"
       "w /proc/sys/dev/hpet/max-user-freq - - - - 3072"
+
+      "w /sys/kernel/mm/lru_gen/min_ttl_ms - - - - 500"
     ];
 
     services."user@".serviceConfig.Delegate = "cpu cpuset io memory pids";
