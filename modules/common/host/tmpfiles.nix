3e8037f7758000754f0e8dde8d60eeda980068f0
pongo1231 <pongo@gopong.dev>
2025-05-25 01:23:37 +0200

    tmpfiles: min_ttl_ms to 1000

--- modules/common/host/tmpfiles.nix
+++ modules/common/host/tmpfiles.nix
@@ -3,5 +3,5 @@
   "w /sys/class/rtc/rtc0/max_user_freq - - - - 3072"
   "w /proc/sys/dev/hpet/max-user-freq - - - - 3072"
 
-  "w /sys/kernel/mm/lru_gen/min_ttl_ms - - - - 500"
+  "w /sys/kernel/mm/lru_gen/min_ttl_ms - - - - 1000"
 ]
