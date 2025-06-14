57e9fb7f226ade93cf928b278b5d3cd430bd0ece
pongo1231 <pongo@gopong.dev>
2025-06-14 14:46:58 +0200

    hp-omen-linux-module: update

--- pkgs/hp-omen-linux-module/default.nix
+++ pkgs/hp-omen-linux-module/default.nix
@@ -5,13 +5,13 @@
 }:
 stdenv.mkDerivation (final: {
   pname = "hp-omen-linux-module";
-  version = "rebase-6.14";
+  version = "rebase-6.15";
 
   src = fetchFromGitHub {
     owner = "ranisalt";
     repo = "hp-omen-linux-module";
     rev = final.version;
-    sha256 = "sha256-2zCm29bdboSjRm/caMjBPGNc0tZXPUnIIYlHxxfhAok=";
+    sha256 = "sha256-IOXHzcCB0n1InMjeIu3XYEJ4bhbHS3NIlS8/+4XIwkQ=";
   };
 
   setSourceRoot = ''
