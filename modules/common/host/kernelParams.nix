e41a2a06ca470fb18f210a7973fe87da144d2672
pongo1231 <pongo@gopong.dev>
2025-06-15 14:10:07 +0200

    sysctl: clean up

--- modules/common/host/kernelParams.nix
+++ modules/common/host/kernelParams.nix
@@ -5,7 +5,6 @@ config: [
   "msr.allow_writes=on"
   "cgroup_no_v1=all"
   "mitigations=off"
-  "split_lock_detect=off"
   "transparent_hugepage=${if config.pongo.pongoKernel.enable then "defer" else "always"}"
   "transparent_hugepage_shmem=always"
   "transparent_hugepage_tmpfs=always"
--- modules/common/host/sysctl.nix
+++ modules/common/host/sysctl.nix
@@ -1,7 +1,20 @@
 {
-  # https://wiki.archlinux.org/title/Zram#Optimizing_swap_on_zram
-  "vm.swappiness" = 200;
+  # https://github.com/CachyOS/CachyOS-Settings/blob/master/usr/lib/sysctl.d/99-cachyos-settings.conf
+  "vm.swappiness" = 150;
+  "vm.vfs_cache_pressure" = 50;
+  "vm.dirty_bytes" = 268435456;
   "vm.page-cluster" = 0;
+  "vm.dirty_background_bytes" = 67108864;
+  "vm.dirty_writeback_centisecs " = 1500;
+  "fs.inotify.max_user_instances" = 1024;
+  "kernel.nmi_watchdog" = 0;
+  "kernel.printk" = "3 3 3 3";
+  "kernel.kptr_restrict" = 2;
+  "kernel.kexec_load_disabled" = 1;
+  "net.core.netdev_max_backlog" = 4096;
+  "fs.file-max" = 2097152;
+
+  # https://wiki.archlinux.org/title/Zram#Optimizing_swap_on_zram
   "vm.watermark_boost_factor" = 0;
   "vm.watermark_scale_factor" = 125;
 
@@ -10,19 +23,6 @@
   "kernel.sysrq" = 1;
   "kernel.core_pattern" = "/dev/null";
 
-  # https://github.com/pop-os/default-settings/blob/master_noble/etc/sysctl.d/10-pop-default-settings.conf
-  "vm.dirty_bytes" = 268435456;
-  "vm.dirty_background_bytes" = 134217728;
-  "fs.inotify.max_user_instances" = 1024;
-
   "vm.compact_unevictable_allowed" = 1;
   "vm.compaction_proactiveness" = 20;
-
-  "kernel.split_lock_mitigate" = 0;
-
-  # Cachyos
-  "vm.vfs_cache_pressure" = 50;
-  "vm.dirty_writeback_centisecs" = 1500;
-  "net.core.netdev_max_backlog" = 4096;
-  "fs.file-max" = 2097152;
 }
