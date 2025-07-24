89d2b2629e86297cda4c150dafdfc2d5b7ae4e6a
pongo1231 <pongo@gopong.dev>
2025-07-24 03:18:44 +0200

    jupiter/home: add packages

--- configs/jupiter/home/default.nix
+++ configs/jupiter/home/default.nix
@@ -1,9 +1,19 @@
 {
   module,
+  pkgs,
   ...
 }:
 {
   imports = [
     (import (module /gpu) [ "amd" ])
+  ];
+
+  home.packages = with pkgs; [
+    mangohud
+    gamescope
+    heroic
+    bottles
+    pcsx2
+    xemu
   ];
 }
