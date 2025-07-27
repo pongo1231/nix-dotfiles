ff8a776d90c7b838b38c7bd5052327a28a72a878
pongo1231 <pongo@gopong.dev>
2025-07-27 19:17:41 +0200

    rpi3b: enable & cross-compile pongoKernel

--- configs/rpi3b/info.nix
+++ configs/rpi3b/info.nix
@@ -1,3 +1,10 @@
 {
   system = "aarch64-linux";
+  host.pongoKernel = {
+    enable = true;
+    crossCompile = {
+      host = "x86_64-linux";
+      target = "aarch64-multiplatform";
+    };
+  };
 }
--- modules/common/host/pongoKernel.nix
+++ modules/common/host/pongoKernel.nix
@@ -1,4 +1,6 @@
 {
+  inputs,
+  system,
   patch,
   config,
   pkgs,
@@ -9,86 +11,100 @@ let
   cfg = config.pongo;
 in
 {
-  options.pongo.pongoKernel.enable = lib.mkOption {
-    type = lib.types.bool;
-    default = false;
+  options.pongo.pongoKernel = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
+
+    crossCompile = lib.mkOption {
+      type = lib.types.nullOr lib.types.attrs;
+      default = null;
+    };
   };
 
   config = lib.mkIf cfg.pongoKernel.enable {
     nixpkgs.overlays = [
       (final: _: {
-        linuxPackages_pongo = final.linuxPackages_testing.extend (
-          _: prev': {
-            kernel =
-              (prev'.kernel.override {
-                /*
-                  stdenv = pkgs.llvmPackages.stdenv.override (prev'': {
-                    cc = prev''.cc.override {
-                      bintools = pkgs.llvmPackages.bintools;
-                      extraBuildCommands = ''
-                                        substituteInPlace "$out/nix-support/cc-cflags" --replace-fail " -nostdlibinc" ""
-                        				  echo " -resource-dir=${pkgs.llvmPackages.libclang.lib}/lib/clang/${lib.versions.major pkgs.llvmPackages.libclang.version}" >> $out/nix-support/cc-cflags
-                        				  '';
-                    };
-                  });
-                */
-
-                #kernelPatches = builtins.filter (x: !lib.hasPrefix "netfilter-typo-fix" x.name) prev'.kernelPatches;
-
-                #ignoreConfigErrors = true;
-
-                argsOverride =
-                  let
-                    version = "6.16.0-git";
-                  in
-                  {
-                    inherit version;
-                    modDirVersion = "6.16.0-rc7";
-                    src = final.fetchFromGitHub {
-                      owner = "pongo1231";
-                      repo = "linux";
-                      rev = "171e8835bca4e6b25261d5d753e5c09bf7a5d134";
-                      hash = "sha256-T/lfzK6DYPX/nUI1oovLtyP6E93Owr4HcKY3kngTS8Q=";
-                    };
-
-                    #src = final.fetchzip {
-                    #    url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-                    #    hash = "";
-                    # };
-
+        linuxPackages_pongo =
+          (
+            if cfg.pongoKernel.crossCompile != null then
+              inputs.nixpkgs.legacyPackages.${cfg.pongoKernel.crossCompile.host}.pkgsCross.${cfg.pongoKernel.crossCompile.target}.linuxPackages_testing
+            else
+              final.linuxPackages_testing
+          ).extend
+            (
+              _: prev': {
+                kernel =
+                  (prev'.kernel.override {
                     /*
-                      extraMakeFlags = [
-                        "LLVM=1"
-                        "LLVM_IAS=1"
-                      ];
-
-                      extraConfig = ''
-                        CC_IS_CLANG y
-                        LTO_CLANG y
-                        LTO_CLANG_THIN y
-                        LTO_CLANG_THIN_DIST y
-                      '';
+                      stdenv = pkgs.llvmPackages.stdenv.override (prev'': {
+                        cc = prev''.cc.override {
+                          bintools = pkgs.llvmPackages.bintools;
+                          extraBuildCommands = ''
+                                            substituteInPlace "$out/nix-support/cc-cflags" --replace-fail " -nostdlibinc" ""
+                            				  echo " -resource-dir=${pkgs.llvmPackages.libclang.lib}/lib/clang/${lib.versions.major pkgs.llvmPackages.libclang.version}" >> $out/nix-support/cc-cflags
+                            				  '';
+                        };
+                      });
                     */
+
+                    #kernelPatches = builtins.filter (x: !lib.hasPrefix "netfilter-typo-fix" x.name) prev'.kernelPatches;
+
+                    #ignoreConfigErrors = true;
+
+                    argsOverride =
+                      let
+                        version = "6.16.0-git";
+                      in
+                      {
+                        inherit version;
+                        modDirVersion = "6.16.0-rc7";
+                        src = final.fetchFromGitHub {
+                          owner = "pongo1231";
+                          repo = "linux";
+                          rev = "171e8835bca4e6b25261d5d753e5c09bf7a5d134";
+                          hash = "sha256-T/lfzK6DYPX/nUI1oovLtyP6E93Owr4HcKY3kngTS8Q=";
+                        };
+
+                        #src = final.fetchzip {
+                        #    url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
+                        #    hash = "";
+                        # };
+
+                        /*
+                          extraMakeFlags = [
+                            "LLVM=1"
+                            "LLVM_IAS=1"
+                          ];
+
+                          extraConfig = ''
+                            CC_IS_CLANG y
+                            LTO_CLANG y
+                            LTO_CLANG_THIN y
+                            LTO_CLANG_THIN_DIST y
+                          '';
+                        */
+                      };
+                  }).overrideAttrs
+                    (_: {
+                      #hardeningDisable = [ "strictoverflow" ];
+                    });
+
+                xpadneo = prev'.xpadneo.overrideAttrs {
+                  src = final.fetchFromGitHub {
+                    owner = "atar-axis";
+                    repo = "xpadneo";
+                    rev = "a16acb03e7be191d47ebfbc8ca1d5223422dac3e";
+                    hash = "sha256-4eOP6qAkD7jGOqaZPOB5/pdoqixl2Jy2iSVvK2caE80=";
                   };
-              }).overrideAttrs
-                (_: {
+
+                  #makeFlags = prev''.makeFlags ++ [ "LLVM=1" ];
+
                   #hardeningDisable = [ "strictoverflow" ];
-                });
-
-            xpadneo = prev'.xpadneo.overrideAttrs {
-              src = final.fetchFromGitHub {
-                owner = "atar-axis";
-                repo = "xpadneo";
-                rev = "a16acb03e7be191d47ebfbc8ca1d5223422dac3e";
-                hash = "sha256-4eOP6qAkD7jGOqaZPOB5/pdoqixl2Jy2iSVvK2caE80=";
-              };
-
-              #makeFlags = prev''.makeFlags ++ [ "LLVM=1" ];
-
-              #hardeningDisable = [ "strictoverflow" ];
-            };
-          }
-        );
+                };
+              }
+            );
       })
     ];
 
@@ -100,13 +116,15 @@ in
           name = "base";
           patch = null;
           extraConfig = ''
+            BTRFS_EXPERIMENTAL y
+          ''
+          + lib.optionalString (system == "x86_&4-linux") ''
             X86_64_VERSION 3
             AMD_PRIVATE_COLOR y
             LEDS_STEAMDECK m
             EXTCON_STEAMDECK m
             MFD_STEAMDECK m
             SENSORS_STEAMDECK m
-            BTRFS_EXPERIMENTAL y
           '';
         }
       ];
