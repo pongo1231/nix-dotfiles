cf7dbf542bc738c255e2684aeee2c94fc135afb6
pongo1231 <pongo@gopong.dev>
2025-08-05 16:14:02 +0200

    sysctl: update

--- modules/common/host/sysctl.nix
+++ modules/common/host/sysctl.nix
@@ -1,12 +1,18 @@
 {
-  "vm.swappiness" = 60;
+  "vm.swappiness" = 200;
+  "vm.vfs_cache_pressure" = 50;
+  "vm.dirty_bytes" = 268435456;
+  "vm.page-cluster" = 0;
+  "vm.dirty_background_bytes" = 67108864;
+  "vm.dirty_writeback_centisecs" = 1500;
   "kernel.nmi_watchdog" = 0;
   "kernel.printk" = "3 3 3 3";
   "kernel.kptr_restrict" = 2;
   "kernel.kexec_load_disabled" = 1;
+  "net.core.netdev_max_backlog" = 4096;
+  "vm.watermark_boost_factor" = 0;
   "vm.watermark_scale_factor" = 200;
   "kernel.sysrq" = 1;
-  "vm.compact_unevictable_allowed" = 1;
+  "vm.compact_unevictable_allowed" = 0;
   "vm.compaction_proactiveness" = 0;
-  "vm.dirty_background_ratio" = 10;
 }
