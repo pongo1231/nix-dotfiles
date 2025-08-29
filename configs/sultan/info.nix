21923ad78debfee8de34a7359c0ccf05cf1c6e10
pongo1231 <pongo@gopong.dev>
2025-08-29 11:47:21 +0200

    sultan: enable forced ksm

--- configs/sultan/info.nix
+++ configs/sultan/info.nix
@@ -1,13 +1,20 @@
 {
   system = "aarch64-linux";
 
-  host.pongoKernel = {
-    enable = true;
-    /*
-      crossCompile = {
-        host = "x86_64-linux";
-        target = "aarch64-multiplatform";
-      };
-    */
+  host = {
+    pongoKernel = {
+      enable = true;
+      /*
+        crossCompile = {
+          host = "x86_64-linux";
+          target = "aarch64-multiplatform";
+        };
+      */
+    };
+
+    ksm = {
+      forceAllProcesses = true;
+      patchSystemd = true;
+    };
   };
 }
