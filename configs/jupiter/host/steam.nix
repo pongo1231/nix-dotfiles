89686192927e6583d717d9e4250159466868fbf7
pongo1231 <pongo@gopong.dev>
2025-08-29 22:52:45 +0200

    jupiter/gamescope: extend refresh rate range

--- configs/jupiter/host/steam.nix
+++ configs/jupiter/host/steam.nix
@@ -3,6 +3,17 @@
   ...
 }:
 {
+  nixpkgs.overlays = [
+    (final: prev: {
+      gamescope = prev.gamescope.overrideAttrs (prev': {
+        postPatch = prev'.postPatch + ''
+		  substituteInPlace scripts/00-gamescope/displays/valve.steamdeck.lcd.lua --replace-fail "40, 41, 42, 43, 44, 45, 46, 47, 48, 49," "30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,"
+		  substituteInPlace scripts/00-gamescope/displays/valve.steamdeck.lcd.lua --replace-fail "        60" "        60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70"
+        '';
+      });
+    })
+  ];
+
   programs.steam.extest.enable = true;
 
   jovian = {
@@ -16,7 +27,7 @@
       autoStart = true;
       user = "pongo";
       desktopSession = "plasma";
-      environment."ENABLE_GAMESCOPE_WSI" = "0";
+      environment.ENABLE_GAMESCOPE_WSI = "0";
     };
 
     decky-loader = {
