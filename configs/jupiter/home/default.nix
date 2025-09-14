47d58fcee84aa1125cfbd7aa82bc0e5c3d8cb121
pongo1231 <pongo@gopong.dev>
2025-09-14 15:30:15 +0200

    home/jupiter: add various packages

--- configs/jupiter/home/default.nix
+++ configs/jupiter/home/default.nix
@@ -1,9 +1,47 @@
 {
   module,
+  pkgs,
   ...
 }:
 {
   imports = [
     (import (module /gpu) [ "amd" ])
   ];
+
+  home = {
+    file = {
+      "pcsx2.sh" = {
+        text = ''
+          #!/bin/sh
+          pcsx2-qt "''${@:1}"
+        '';
+        executable = true;
+      };
+
+      "ppsspp.sh" = {
+        text = ''
+          #!/bin/sh
+          ppsspp "''${@:1}"
+        '';
+        executable = true;
+      };
+
+      "xemu.sh" = {
+        text = ''
+          #!/bin/sh
+          xemu "''${@:1}"
+        '';
+        executable = true;
+      };
+    };
+
+    packages = with pkgs; [
+      pcsx2
+      ppsspp
+      xemu
+      heroic
+      (bottles.override { removeWarningPopup = true; })
+      atlauncher
+    ];
+  };
 }
