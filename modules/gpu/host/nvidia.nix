7d3ac4d52c53529dc341b3498742b7034ff5a09c
pongo1231 <pongo@gopong.dev>
2025-09-18 20:21:29 +0200

    nvidia: update

--- modules/gpu/host/nvidia.nix
+++ modules/gpu/host/nvidia.nix
@@ -23,10 +23,10 @@
         {
           nvidiaPackages.beta =
             (generic {
-              version = "580.82.07";
-              sha256_64bit = "sha256-Bh5I4R/lUiMglYEdCxzqm3GLolQNYFB0/yJ/zgYoeYw=";
-              openSha256 = "sha256-8/7ZrcwBMgrBtxebYtCcH5A51u3lAxXTCY00LElZz08=";
-              settingsSha256 = "sha256-lx1WZHsW7eKFXvi03dAML6BoC5glEn63Tuiz3T867nY=";
+              version = "580.82.09";
+              sha256_64bit = "sha256-Puz4MtouFeDgmsNMKdLHoDgDGC+QRXh6NVysvltWlbc=";
+              openSha256 = "sha256-YB+mQD+oEDIIDa+e8KX1/qOlQvZMNKFrI5z3CoVKUjs=";
+              settingsSha256 = "sha256-um53cr2Xo90VhZM1bM2CH4q9b/1W2YOqUcvXPV6uw2s=";
               persistencedSha256 = "";
               patches = [
 
@@ -40,21 +40,23 @@
 
                 passthru = prev'.passthru // {
                   open = prev'.passthru.open.overrideAttrs (prev'': {
-                    patches = [
-                      (pkgs.fetchpatch {
-                        url = "https://gist.githubusercontent.com/sharkautarch/4e63bbdcb27aafb0bc755f35cf77e69a/raw/d2f7a22c21b9dce2c6da0710ffc8868c13c002df/0002-workaround-kcfi-issues.patch";
-                        hash = "sha256-PoMcu6KZcjx1F9ciaBM5VJNPD8bLsihFVruAtjXJgWI=";
-                      })
-                    ];
+                    /*
+                      patches = [
+                        (pkgs.fetchpatch {
+                          url = "https://gist.githubusercontent.com/sharkautarch/4e63bbdcb27aafb0bc755f35cf77e69a/raw/d2f7a22c21b9dce2c6da0710ffc8868c13c002df/0002-workaround-kcfi-issues.patch";
+                          hash = "sha256-PoMcu6KZcjx1F9ciaBM5VJNPD8bLsihFVruAtjXJgWI=";
+                        })
+                      ];
+                    */
 
                     makeFlags =
                       prev''.makeFlags
                       ++ final.kernel.extraMakeFlags
                       ++ [
-                        #"CC=${pkgs.llvmPackages_20.stdenv.cc}/bin/clang"
+                        #"CC=${pkgs.llvmPackages_21.stdenv.cc}/bin/clang"
                       ];
 
-                    NIX_CFLAGS_COMPILE = "-O3 -flto=thin -march=x86-64-v3 -fsanitize=kcfi -Wno-error=unused-command-line-argument";
+                    NIX_CFLAGS_COMPILE = "-O3 -flto=thin -march=x86-64-v3 -Wno-error=unused-command-line-argument";
                   });
                 };
               });
@@ -83,6 +85,5 @@
 
   boot.extraModprobeConfig = ''
     options nvidia NVreg_UsePageAttributeTable=1 NVreg_InitializeSystemMemoryAllocations=0 NVreg_EnablePCIeGen3=1 NVreg_RegistryDwords=RMIntrLockingMode=1
-    options nvidia_drm fbdev=0
   '';
 }
