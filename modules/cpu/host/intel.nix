2d7e61ec0daad2ba218c4d07b324cf3e35193e57
pongo1231 <pongo@gopong.dev>
2025-09-20 22:35:19 +0200

    cpu/intel: add intel-undervolt

--- modules/cpu/host/intel.nix
+++ modules/cpu/host/intel.nix
@@ -1,8 +1,13 @@
-_: {
+{ pkgs, ... }:
+{
   boot.kernelParams = [
     "intel_iommu=on"
     "iommu=pt"
   ];
 
   services.thermald.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    intel-undervolt
+  ];
 }
