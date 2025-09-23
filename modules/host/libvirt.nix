00f72c82bdba57912fde4f6c21d48e270ec8d802
pongo1231 <pongo@gopong.dev>
2025-09-23 21:42:13 +0200

    libvirt: drop custom qemu package

--- modules/host/libvirt.nix
+++ modules/host/libvirt.nix
@@ -8,28 +8,6 @@
     libvirtd = {
       enable = true;
       qemu = {
-        package =
-          (pkgs.qemu_kvm.override (
-            prev:
-            let
-              stdenv = prev.buildPackages.llvmPackages_21.stdenv.override {
-                cc = prev.buildPackages.llvmPackages_21.clang.override {
-                  inherit (prev.buildPackages.llvmPackages_21) bintools;
-                };
-              };
-            in
-            {
-              buildPackages = prev.buildPackages // {
-                inherit stdenv;
-              };
-
-              inherit stdenv;
-            }
-          )).overrideAttrs
-            {
-              NIX_CFLAGS_COMPILE = "-O3 -flto=thin -march=x86-64-v3";
-            };
-
         verbatimConfig = ''
           cgroup_device_acl = [
             "/dev/null", "/dev/full", "/dev/zero",
