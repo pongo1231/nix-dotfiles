776ef663d5d360c8714f6f50d7190bf830bd8fcb
pongo1231 <pongo@gopong.dev>
2025-09-23 21:42:37 +0200

    desktop: drop custom mesa package

--- modules/desktop/host/default.nix
+++ modules/desktop/host/default.nix
@@ -13,64 +13,7 @@
   };
 
   hardware = {
-    graphics =
-      let
-        patchMesa =
-          mesa:
-          (mesa.override (
-            prev:
-            let
-              stdenv = prev.buildPackages.llvmPackages_latest.stdenv.override {
-                cc = prev.buildPackages.llvmPackages_latest.clang.override {
-                  inherit (prev.buildPackages.llvmPackages_latest) bintools;
-                };
-              };
-            in
-            {
-              buildPackages = prev.buildPackages // {
-                inherit stdenv;
-              };
-
-              inherit stdenv;
-
-              llvmPackages = prev.buildPackages.llvmPackages_latest // {
-                inherit (prev.llvmPackages) libclc;
-              };
-
-              spirv-llvm-translator = prev.spirv-llvm-translator.override {
-                inherit (prev.buildPackages.llvmPackages_latest) llvm;
-              };
-
-              galliumDrivers = [
-                "radeonsi"
-                "iris"
-                "virgl"
-                "zink"
-                "llvmpipe"
-              ];
-
-              vulkanDrivers = [
-                "amd"
-                "intel"
-                "gfxstream"
-                "swrast"
-                "virtio"
-              ];
-            }
-          )).overrideAttrs
-            (prev: {
-              NIX_CFLAGS_COMPILE = "-O3 -flto=thin";
-
-              outputs = lib.filter (x: x != "spirv2dxil") prev.outputs;
-            });
-      in
-      {
-        enable = true;
-        #enable32Bit = true;
-
-        package = lib.mkForce (patchMesa pkgs.mesa);
-        #package32 = lib.mkForce (patchMesa pkgs.pkgsi686Linux.mesa);
-      };
+    graphics.enable = true;
 
     xpadneo.enable = true;
   };
