93d595a76c76eb4e10f832bac14be8b7767b55f2
pongo1231 <pongo@gopong.dev>
2025-10-02 18:42:54 +0200

    flake: update revs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:pongo1231/nixpkgs/mine";
     nixpkgs2.follows = "nixpkgs";
 
     flake-utils.url = "github:numtide/flake-utils";
