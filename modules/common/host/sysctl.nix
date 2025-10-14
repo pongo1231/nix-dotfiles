a888fbf9f288ded1574534aed05e6e661576a1e7
pongo1231 <pongo@gopong.dev>
2025-10-14 20:21:58 +0200

    sysctl: revert recent changes

--- modules/common/host/sysctl.nix
+++ modules/common/host/sysctl.nix
@@ -1,22 +1,8 @@
-config:
-{
+config: {
   "kernel.nmi_watchdog" = 0;
   "kernel.printk" = "3 3 3 3";
   "kernel.kptr_restrict" = 2;
   "kernel.sysrq" = 1;
+  "vm.page-cluster" = 0;
+  "vm.watermark_boost_factor" = 0;
 }
-// (
-  if config.pongo.pongoKernel.enable then
-    { }
-  else
-    {
-      "vm.page-cluster" = 0;
-      "vm.watermark_boost_factor" = 0;
-      "vm.compact_unevictable_allowed" = 0;
-      "vm.compaction_proactiveness" = 0;
-      "vm.swappiness" = 100;
-      "vm.dirty_writeback_centisecs" = 1000;
-      "vm.dirty_background_ratio" = 5;
-      "kernel.sched_cfs_bandwidth_slice_us" = 3000;
-    }
-)
