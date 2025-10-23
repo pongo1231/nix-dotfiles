1f9f2ae01a938c891d84a9f88ef5143525b5f92c
pongo1231 <pongo@gopong.dev>
2025-10-23 21:44:04 +0200

    desktop/plasma: add KWIN_USE_OVERLAYS env var

--- modules/desktop/plasma/host/default.nix
+++ modules/desktop/plasma/host/default.nix
@@ -16,27 +16,31 @@
     desktopManager.plasma6.enable = lib.mkDefault true;
   };
 
-  environment.systemPackages =
-    with pkgs;
-    with pkgs.kdePackages;
-    [
-      kcmutils
-      sddm-kcm
-      flatpak-kcm
-      kate
-      ark
-      kcalc
-      ocs-url
-      krfb # for the "Virtual Display" button in kde connect to work
-      maliit-keyboard
-      qtstyleplugin-kvantum
+  environment = {
+    sessionVariables."KWIN_USE_OVERLAYS" = 1;
 
-      # for KDE info center
-      clinfo
-      glxinfo
-      vulkan-tools
-      wayland-utils
-      aha
-      dmidecode
-    ];
+    systemPackages =
+      with pkgs;
+      with pkgs.kdePackages;
+      [
+        kcmutils
+        sddm-kcm
+        flatpak-kcm
+        kate
+        ark
+        kcalc
+        ocs-url
+        krfb # for the "Virtual Display" button in kde connect to work
+        maliit-keyboard
+        qtstyleplugin-kvantum
+
+        # for KDE info center
+        clinfo
+        glxinfo
+        vulkan-tools
+        wayland-utils
+        aha
+        dmidecode
+      ];
+  };
 }
