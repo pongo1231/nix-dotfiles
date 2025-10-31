0724a018ac0a7ea7a6e36d0f60cdd85383c37254
pongo1231 <pongo@gopong.dev>
2025-10-31 01:34:00 +0100

    kernelParams: update

--- modules/common/host/kernelParams.nix
+++ modules/common/host/kernelParams.nix
@@ -9,16 +9,6 @@ config: [
   "transparent_hugepage=always"
   "transparent_hugepage_shmem=within_size"
   "transparent_hugepage_tmpfs=within_size"
-  "preempt=full"
-  "threadirqs"
-  "nohz_full=all"
-  "rcu_nocbs=0-N"
-  "rcutree.enable_rcu_lazy=1"
   "log_buf_len=4M"
   "split_lock_detect=off"
-  "panic=-1"
-
-  # Hardening
-  "randomize_kstack_offset=on"
-  "efi=disable_early_pci_dma"
 ]
