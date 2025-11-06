f4a4f1092cee5d4f20c57b5d8cf647d0fad01577
pongo1231 <pongo@gopong.dev>
2025-11-06 17:52:02 +0100

    nvidia: update driver

--- modules/gpu/host/nvidia.nix
+++ modules/gpu/host/nvidia.nix
@@ -23,10 +23,10 @@
         {
           nvidiaPackages.beta =
             (generic {
-              version = "580.95.05";
-              sha256_64bit = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
-              openSha256 = "sha256-RFwDGQOi9jVngVONCOB5m/IYKZIeGEle7h0+0yGnBEI=";
-              settingsSha256 = "sha256-F2wmUEaRrpR1Vz0TQSwVK4Fv13f3J9NJLtBe4UP2f14=";
+              version = "580.105.08";
+              sha256_64bit = "sha256-2cboGIZy8+t03QTPpp3VhHn6HQFiyMKMjRdiV2MpNHU=";
+              openSha256 = "sha256-FGmMt3ShQrw4q6wsk8DSvm96ie5yELoDFYinSlGZcwQ=";
+              settingsSha256 = "sha256-YvzWO1U3am4Nt5cQ+b5IJ23yeWx5ud1HCu1U0KoojLY=";
               persistencedSha256 = "";
               patches = [
 
