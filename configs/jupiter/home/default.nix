58aace95c6e99d64fb5483e5fcb0f1f3ccf71457
pongo1231 <pongo@gopong.dev>
2025-11-12 02:53:11 +0100

    jupiter: move lsfg* scripts to home & add lsfg1x

--- configs/jupiter/home/default.nix
+++ configs/jupiter/home/default.nix
@@ -38,6 +38,19 @@
       heroic
       (bottles.override { removeWarningPopup = true; })
       atlauncher
+
+      (pkgs.writeShellScriptBin "lsfg1x" ''
+        exec env LSFG_LEGACY= "$@"
+      '')
+      (pkgs.writeShellScriptBin "lsfg2x" ''
+        exec env LSFG_LEGACY=1 LSFG_PERFORMANCE_MODE=1 LSFG_MULTIPLIER=2 "$@"
+      '')
+      (pkgs.writeShellScriptBin "lsfg3x" ''
+        exec env LSFG_LEGACY=1 LSFG_PERFORMANCE_MODE=1 LSFG_MULTIPLIER=3 "$@"
+      '')
+      (pkgs.writeShellScriptBin "lsfg4x" ''
+        exec env LSFG_LEGACY=1 LSFG_PERFORMANCE_MODE=1 LSFG_MULTIPLIER=4 "$@"
+      '')
     ];
   };
 }
