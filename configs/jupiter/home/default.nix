0b41497efe972cb7ef8b7acd5b1da9d3c860805f
pongo1231 <pongo@gopong.dev>
2025-11-21 15:52:23 +0100

    home/jupiter: update lsfg1x script

--- configs/jupiter/home/default.nix
+++ configs/jupiter/home/default.nix
@@ -40,7 +40,7 @@
       atlauncher
 
       (pkgs.writeShellScriptBin "lsfg1x" ''
-        exec env LSFG_LEGACY= "$@"
+        exec env LSFG_LEGACY=0 LSFG_PERFORMANCE_MODE=1 LSFG_MULTIPLIER=1 "$@"
       '')
       (pkgs.writeShellScriptBin "lsfg2x" ''
         exec env LSFG_LEGACY=1 LSFG_PERFORMANCE_MODE=1 LSFG_MULTIPLIER=2 "$@"
