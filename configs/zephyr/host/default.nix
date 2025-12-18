2cef20ddd787011dba5f41b2cee22d4b61ab4a56
pongo1231 <pongo@gopong.dev>
2025-12-18 20:03:07 +0100

    zephyr: back to i915
    xe is more prone to stuttering under system load
    
    This reverts commit 65478fdefc0a1711ebba7f56ba1e75b53428d498.

--- configs/zephyr/host/default.nix
+++ configs/zephyr/host/default.nix
@@ -49,8 +49,8 @@
     ];
 
     kernelParams = [
-      "i915.force_probe=!7d51"
-      "xe.force_probe=7d51"
+      #"i915.force_probe=!7d51"
+      #"xe.force_probe=7d51"
       "irqaffinity=14,15"
     ];
 
