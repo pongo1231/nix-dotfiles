53e4f6968097528d439c6eafbc7dc7c6d863bbce
pongo1231 <pongo@gopong.dev>
2026-01-16 15:05:27 +0100

    gopong/hastebin: make password protected

--- configs/gopong/host/hastebin.nix
+++ configs/gopong/host/hastebin.nix
@@ -1,5 +1,6 @@
 {
   config,
+  pkgs,
   ...
 }:
 {
@@ -8,6 +9,7 @@
       forceSSL = true;
       useACMEHost = config.networking.fqdn;
       locations."/".proxyPass = "http://localhost:${toString config.services.haste-server.settings.port}";
+      basicAuthFile = pkgs.writeText ".htpasswd" "haste:$apr1$4LvrNjr2$ywj.vBUiRw3LhmwiunVSU1";
     };
 
     haste-server.enable = true;
