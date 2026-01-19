465ab5febba4193eb4a94c37c56da39cf851061e
pongo1231 <pongo@gopong.dev>
2026-01-19 15:56:41 +0100

    gopong/nextcloud: drop activation script & specify mail options

--- configs/gopong/host/nextcloud.nix
+++ configs/gopong/host/nextcloud.nix
@@ -2,75 +2,85 @@
   withSecrets,
   config,
   pkgs,
+  lib,
   ...
 }:
-withSecrets "pongo"
-  {
-    store = "gopong/default.yaml";
-    owner = config.users.users.nextcloud.name;
-  }
-  {
-    "nextcloud/adminPassword" = { };
-  }
-// {
-  services = {
-    nginx.virtualHosts = {
-      "cloud.${config.networking.fqdn}" = {
-        forceSSL = true;
-        useACMEHost = config.networking.fqdn;
-      };
+lib.mkMerge [
+  (withSecrets "pongo"
+    {
+      store = "gopong/default.yaml";
+      owner = config.users.users.nextcloud.name;
+    }
+    {
+      "nextcloud/adminPassword" = { };
+    }
+  )
 
-      "collabora.${config.networking.fqdn}" = {
-        forceSSL = true;
-        useACMEHost = config.networking.fqdn;
-        locations."/" = {
-          proxyPass = "https://localhost:9980";
-          proxyWebsockets = true;
+  (withSecrets "pongo"
+    {
+      store = "gopong/nextcloud.json";
+      owner = config.users.users.nextcloud.name;
+    }
+    {
+      "nextcloudfile".key = "";
+    }
+  )
+
+  {
+    services = {
+      nginx.virtualHosts = {
+        "cloud.${config.networking.fqdn}" = {
+          forceSSL = true;
+          useACMEHost = config.networking.fqdn;
+        };
+
+        "collabora.${config.networking.fqdn}" = {
+          forceSSL = true;
+          useACMEHost = config.networking.fqdn;
+          locations."/" = {
+            proxyPass = "https://localhost:9980";
+            proxyWebsockets = true;
+          };
         };
       };
-    };
 
-    redis.servers."nextcloud" = {
-      enable = true;
-      port = 6501;
-    };
+      nextcloud = {
+        enable = true;
+        package = pkgs.nextcloud32;
+        hostName = "cloud.${config.networking.fqdn}";
+        https = true;
 
-    nextcloud = {
-      enable = true;
-      package = pkgs.nextcloud32;
-      hostName = "cloud.${config.networking.fqdn}";
-      https = true;
-      config = {
-        adminpassFile = config.sops.secrets."nextcloud/adminPassword".path;
-        dbtype = "pgsql";
+        config = {
+          adminpassFile = config.sops.secrets."nextcloud/adminPassword".path;
+          dbtype = "pgsql";
+        };
+
+        caching = {
+          apcu = false;
+          redis = true;
+          memcached = false;
+        };
+
+        settings = {
+          "mail_domain" = "${config.mailserver.fqdn}";
+          "mail_from_address" = "no-reply";
+          "mail_smtphost" = "${config.mailserver.fqdn}";
+          "mail_smtpport" = 465;
+          "mail_smtpauth" = true;
+          "mail_smtpsecure" = "ssl";
+        };
+
+        secretFile = config.sops.secrets."nextcloudfile".path;
       };
-      caching = {
-        apcu = false;
-        redis = true;
-        memcached = false;
-      };
     };
-  };
 
-  virtualisation.oci-containers.containers.collabora = {
-    image = "docker.io/collabora/code";
-    ports = [ "9980:9980" ];
-    autoStart = true;
-    environment.aliasgroup1 = "https://${config.services.nextcloud.hostName}:443";
-  };
+    virtualisation.oci-containers.containers.collabora = {
+      image = "docker.io/collabora/code";
+      ports = [ "9980:9980" ];
+      autoStart = true;
+      environment.aliasgroup1 = "https://${config.services.nextcloud.hostName}:443";
+    };
 
-  environment.systemPackages = with pkgs; [ ffmpeg ];
-
-  system.activationScripts.nextcloud-config.text =
-    let
-      occ = "${config.services.nextcloud.occ}/bin/nextcloud-occ";
-    in
-    ''
-      ${occ} config:system:set redis 'host' --value 'localhost' --type string
-      ${occ} config:system:set redis 'port' --value ${
-        builtins.toString config.services.redis.servers."nextcloud".port
-      } --type integer
-      ${occ} config:system:set memcache.local --value '\OC\Memcache\Redis' --type string
-      ${occ} config:system:set memcache.locking --value '\OC\Memcache\Redis' --type string
-    '';
-}
+    environment.systemPackages = with pkgs; [ ffmpeg ];
+  }
+]
