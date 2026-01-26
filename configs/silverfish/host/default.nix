32c351044ec16a762585297745ffb7175c4e71ea
pongo1231 <pongo@ecmec.eu>
2026-01-26 14:04:10 +0100

    silverfish: init

---
+++ configs/silverfish/host/default.nix
@@ -0,0 +1,41 @@
+{
+  modulesPath,
+  ...
+}:
+{
+  imports = [ "${modulesPath}/profiles/qemu-guest.nix" ];
+
+  boot = {
+    initrd = {
+      availableKernelModules = [
+        "ata_piix"
+        "uhci_hcd"
+        "virtio_pci"
+        "sr_mod"
+        "virtio_blk"
+      ];
+
+      kernelModules = [ "kvm-amd" ];
+    };
+
+    loader = {
+      systemd-boot.enable = false;
+      grub = {
+        enable = true;
+        device = "/dev/vda";
+      };
+    };
+
+    kernelParams = [ "mitigations=off" ];
+  };
+
+  fileSystems."/" = {
+    device = "/dev/vda1";
+    fsType = "btrfs";
+    options = [
+      "noatime"
+      "lazytime"
+      "compress-force=zstd"
+    ];
+  };
+}
