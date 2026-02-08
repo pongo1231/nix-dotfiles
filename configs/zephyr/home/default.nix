d34a424dad52c804c0cd9acb1aed30e577018771
pongo1231 <pongo@ecmec.eu>
2026-02-08 04:37:00 +0100

    zephyr/home: add packages

--- configs/zephyr/home/default.nix
+++ configs/zephyr/home/default.nix
@@ -7,5 +7,10 @@
   home.packages = with pkgs; [
     inputs.kwin-effects-forceblur.packages.${pkgs.stdenv.hostPlatform.system}.default
     winboat
+    steam
+    vscode-fhs
+    atlauncher
+    ghidra
+    bottles
   ];
 }
