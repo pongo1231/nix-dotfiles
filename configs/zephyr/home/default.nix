e2370c1cec2dde0ff5a150b919d364086d843101
pongo1231 <pongo@ecmec.eu>
2026-02-09 22:49:44 +0100

    zephyr/home: add mangohud

--- configs/zephyr/home/default.nix
+++ configs/zephyr/home/default.nix
@@ -4,6 +4,11 @@
   ...
 }:
 {
+  programs.mangohud = {
+    enable = true;
+    enableSessionWide = true;
+  };
+
   home.packages = with pkgs; [
     inputs.kwin-effects-forceblur.packages.${pkgs.stdenv.hostPlatform.system}.default
     winboat
