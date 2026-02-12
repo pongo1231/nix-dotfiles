6d2d669ac32c7504e0ab43b3ac8bb0b88e74d7ed
pongo1231 <pongo@ecmec.eu>
2026-02-12 21:23:48 +0100

    host: remove physlock

--- common/host/default.nix
+++ common/host/default.nix
@@ -151,11 +151,6 @@ args:
     kmscon.enable = true;
 
     userborn.enable = true;
-
-    physlock = {
-      enable = true;
-      allowAnyUser = true;
-    };
   }
   // lib.optionalAttrs (pkgs.stdenv.hostPlatform.system == "x86_64-linux") {
     scx-loader = {
