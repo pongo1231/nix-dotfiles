70739ec49bd3a846f8052eba9aa480f83ec78678
pongo1231 <pongo@ecmec.eu>
2026-02-25 10:50:29 +0100

    gopong/nextcloud: update to 33

--- configs/gopong/host/nextcloud.nix
+++ configs/gopong/host/nextcloud.nix
@@ -46,7 +46,7 @@ lib.mkMerge [
 
       nextcloud = {
         enable = true;
-        package = pkgs.nextcloud32;
+        package = pkgs.nextcloud33;
         hostName = "cloud.${config.networking.fqdn}";
         https = true;
 
