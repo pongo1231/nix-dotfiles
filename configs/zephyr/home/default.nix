29d75c973f508a74d31575ab322488ae18d64fed
pongo1231 <pongo@ecmec.eu>
2026-03-21 20:45:53 +0100

    flake: update revs

--- configs/zephyr/home/default.nix
+++ configs/zephyr/home/default.nix
@@ -10,8 +10,9 @@
   };
 
   home.packages = with pkgs; [
-    winboat
-    steam-millennium
+    inputs.nixpkgs2.legacyPackages.${pkgs.stdenv.hostPlatform.system}.winboat
+    #steam-millennium
+    steam
     vscodium
     prismlauncher
     ghidra
