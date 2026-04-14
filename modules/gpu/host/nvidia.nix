05e1883a99aa34613ea0b8da163da95c155a1807
pongo1231 <pongo@ecmec.eu>
2026-04-15 00:48:05 +0200

    nvidia: remove nouveau modprobe blacklist kernel param

--- modules/gpu/host/nvidia.nix
+++ modules/gpu/host/nvidia.nix
@@ -71,11 +71,7 @@
     dynamicBoost.enable = true;
   };
 
-  boot = {
-    kernelParams = [ "modprobe.blacklist=nouveau" ];
-
-    extraModprobeConfig = ''
-      options nvidia NVreg_UsePageAttributeTable=1 NVreg_InitializeSystemMemoryAllocations=0 NVreg_EnablePCIeGen3=1 NVreg_RegistryDwords=RMIntrLockingMode=1
-    '';
-  };
+  boot.extraModprobeConfig = ''
+    options nvidia NVreg_UsePageAttributeTable=1 NVreg_InitializeSystemMemoryAllocations=0 NVreg_EnablePCIeGen3=1 NVreg_RegistryDwords=RMIntrLockingMode=1
+  '';
 }
