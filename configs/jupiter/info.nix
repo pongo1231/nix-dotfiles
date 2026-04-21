7175f0444c8fce1ea36f528db2481f36959716db
pongo1231 <pongo@ecmec.eu>
2026-04-21 11:47:39 +0200

    jupiter: re-enable uutils

--- configs/jupiter/info.nix
+++ configs/jupiter/info.nix
@@ -1,6 +1,3 @@
 {
   roles = [ "graphical/plasma" ];
-
-  # sddm fails to start gamescope-session (Authentication error: SDDM::Auth::ERROR_INTERNAL "Child process set up failed: execve: No such file or directory")
-  overlay.enableUutils = false;
 }
