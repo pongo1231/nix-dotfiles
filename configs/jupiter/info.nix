40de255a08b4517c308c1b7993673e4e9fe5594d
pongo1231 <pongo@ecmec.eu>
2026-04-25 03:18:31 +0200

    Revert "jupiter: re-enable uutils"
    This reverts commit 7175f0444c8fce1ea36f528db2481f36959716db.

--- configs/jupiter/info.nix
+++ configs/jupiter/info.nix
@@ -1,3 +1,6 @@
 {
   roles = [ "graphical/plasma" ];
+
+  # sddm fails to start gamescope-session (Authentication error: SDDM::Auth::ERROR_INTERNAL "Child process set up failed: execve: No such file or directory")
+  overlay.enableUutils = false;
 }
