44cb070b690f51a831846bbbf8fc64b2507fade2
pongo1231 <pongo@ecmec.eu>
2026-04-26 05:32:10 +0200

    rpi3b: re-enable pongoKernel

--- configs/rpi3b/info.nix
+++ configs/rpi3b/info.nix
@@ -4,12 +4,9 @@
   host = {
     # install bootloader step fails with rust uutils atm
     overlay.enableUutils = false;
-    pongoKernel = {
-      enable = false;
-      crossCompile = {
-        host = "x86_64-linux";
-        target = "aarch64-multiplatform";
-      };
+    pongoKernel.crossCompile = {
+      host = "x86_64-linux";
+      target = "aarch64-multiplatform";
     };
   };
 }
