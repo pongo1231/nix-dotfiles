c3525bac6929fda14eee46b9a4de9b5194e8e2ea
pongo1231 <pongo@ecmec.eu>
2026-05-21 20:38:01 +0200

    zephyr: disable scx-loader

--- configs/zephyr/host/default.nix
+++ configs/zephyr/host/default.nix
@@ -140,10 +140,13 @@
       };
     };
 
-    scx-loader.settings = {
-      default_sched = "scx_bpfland";
-      default_mode = "LowLatency";
-    };
+    scx-loader.enable = false;
+    /*
+      scx-loader.settings = {
+        default_sched = "scx_bpfland";
+        default_mode = "LowLatency";
+      };
+    */
 
     #opensnitch.enable = true;
 
